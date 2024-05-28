63a9fafea24c334961af6c780728ad6a03068c7c
Kristina Tripak <tinatripak2002@gmail.com>
2024-05-28 15:05:07 +0300

    fixed a review's carousel

--- src/pages/Reviews/Reviews.tsx
+++ src/pages/Reviews/Reviews.tsx
@@ -4,36 +4,62 @@ import ScrollCarousel from 'scroll-carousel-react'
 import styles from './Reviews.module.scss'
 import { CgQuote } from 'react-icons/cg'
 import { useTranslation } from 'react-i18next'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 import reviewlist from './reviewlist'
 
 const Reviews: React.FC = () => {
   const { t, i18n } = useTranslation()
+  const slider = React.useRef(null)
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1290,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 855,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
 
   return (
     <Layout>
       <div className={styles.reviews}>
         <p className={styles.heading}>{t('reviews_title')}</p>
-        <ScrollCarousel
-          autoplay
-          autoplaySpeed={1}
-          speed={1}
-          className={styles.carousel}
-        >
+        <Slider {...settings} ref={slider} className={styles.carousel}>
           {reviewlist.map((review) => (
-            <div key={review.id} className={styles.review}>
-              <p className={styles.name}>
-                {i18n.language == 'en' ? review.name : review.nameUkr}
-              </p>
-              <CgQuote />
-              <p className={styles.text}>
-                {i18n.language == 'en'
-                  ? review.reviewText
-                  : review.reviewTextUkr}
-              </p>
+            <div key={review.id} className={styles.slide}>
+              <div className={styles.review}>
+                <p className={styles.name}>
+                  {i18n.language == 'en' ? review.name : review.nameUkr}
+                </p>
+                <CgQuote />
+                <p className={styles.text}>
+                  {i18n.language == 'en'
+                    ? review.reviewText
+                    : review.reviewTextUkr}
+                </p>
+              </div>
             </div>
           ))}
-        </ScrollCarousel>
+        </Slider>
         <div className={styles.quote}>
           <q>{t('reviews_quote')}</q>
           <br />— {t('reviews_quotePerson')}
