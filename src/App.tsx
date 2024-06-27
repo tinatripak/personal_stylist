ff48e6e7d460dd53f2e9bf8c4c7b44ad7338e6f7
Kristina Tripak <tinatripak2002@gmail.com>
2024-06-27 14:42:32 -0400

    fixed scrolldown

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,42 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, ReactNode, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Loader from './components/Loader/Loader'
 import './styles/index.scss'
+import useScrollToTop from './ScrollDown'
 
 const Home = lazy(() => import('./pages/Home/Home'))
 const Values = lazy(() => import('./pages/Values/Values'))
 const Services = lazy(() => import('./pages/Services/Services'))
 const Service = lazy(() => import('./pages/Services/Service/Service'))
-const Reviews = lazy(() => import('./pages/Reviews/Reviews'))
 const Products = lazy(() => import('./pages/Products/Products'))
 const RefundPolicy = lazy(() => import('./pages/RefundPolicy/RefundPolicy'))
 
-const App: React.FC = () => (
-  <Router>
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="values" element={<Values />} />
-        <Route path="services" element={<Services />} />
-        <Route path="service/:id" element={<Service />} />
-        {/* <Route path="reviews" element={<Reviews />} /> */}
-        <Route path="products" element={<Products />} />
-        <Route path="refundPolicy" element={<RefundPolicy />} />
-      </Routes>
-    </Suspense>
-  </Router>
-)
+interface ScrollHandlerProps {
+  children: ReactNode
+}
+
+const ScrollHandler: React.FC<ScrollHandlerProps> = ({ children }) => {
+  useScrollToTop()
+  return <>{children}</>
+}
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <ScrollHandler>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="values" element={<Values />} />
+            <Route path="services" element={<Services />} />
+            <Route path="service/:id" element={<Service />} />
+            <Route path="products" element={<Products />} />
+            <Route path="refundPolicy" element={<RefundPolicy />} />
+          </Routes>
+        </ScrollHandler>
+      </Suspense>
+    </Router>
+  )
+}
+
 export default App
